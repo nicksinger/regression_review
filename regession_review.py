f5c03ad7b68901cdb538fb437a97e590dc228ba2
Nick Singer <nsinger@suse.de>
2018-03-23 11:38:36 +0100

    Add bug references to job tree

--- regession_review.py
+++ regession_review.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import pdb
+import re
 
 BASE_API_URL = "https://openqa.suse.de/api/v1"
 
@@ -21,6 +22,25 @@ def getJobs(build, groupid):
 	req = req.json()
 	return req["jobs"] # There is only "jobs" in there
 
+def getLabelComment(jobid):
+	req = requests.get(BASE_API_URL + "/jobs/" + jobid + "/comments")
+	req = req.json()
+	if len(req) >= 1:
+		return req[0]["renderedMarkdown"].replace("\n", "").strip()
+	else:
+		return ""
+
+def extractTrackerLink(comment):
+	# TODO: replace the regex with BeautifulSoup - Because it is never, ever, on no planet in our universe, a good idea to parse html with regex…
+	regex = re.search(r"""(<a).*"((http|https).*(bugzilla\.suse\.com|progress\.opensuse\.org).*?)".*?(<\/a>)""", comment)
+	try:
+		link = regex.group(2) # this is the big, outer match group which contains the full link
+	except AttributeError: # if the regex didn't match, regex is from type None
+		return comment
+	else:
+		return link
+
+
 def getMostRecentBuild(groupid):
 	# TODO: *.json routes are not considered stable - find an alternative
 	req = requests.get("https://openqa.suse.de/group_overview/" + str(groupid) + ".json")
@@ -32,7 +52,15 @@ def main():
 	build = getMostRecentBuild(112)
 	jobs = getJobs(build, 112)
 	for job in jobs:
-		print(job["name"])
+		result = job["result"]
+		if result == "passed":
+			color = bcolors.OKGREEN
+		elif result == "failed":
+			color = bcolors.FAIL
+			trackerLink = " (" + extractTrackerLink(getLabelComment(str(job["id"]))) + ")"
+
+		print(color + job["name"] + bcolors.ENDC + trackerLink)
+
 		modules = job["modules"]
 		for module in modules:
 			color = bcolors.ENDC # default to no color
